496b8eb4d2ec3c08c0513e988d9f01b0ff743fdb
Nan Andrei David <andrei.nan03@e-uvt.ro>
2023-05-17 14:50:30 +0300

    Added check for direction pressed;

--- src/main/java/src/Board.java
+++ src/main/java/src/Board.java
@@ -1,9 +1,19 @@
 package src;
 
+
+
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.ObservableList;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
+
 import javafx.scene.layout.TilePane;
 
 import java.util.Random;
+
+import static javafx.scene.input.KeyCode.O;
+import static src.Direction.*;
 
 public class Board extends TilePane {
 
@@ -51,7 +61,7 @@ public class Board extends TilePane {
     private void addNewTile() {
         ObservableList<Slot> emptySlots = getSlotsWithEmptyTiles();
 
-    //    assert emptySlots != null;
+        //assert emptySlots != null;
         int numOfEmptySlots = emptySlots.size();
 
         if (numOfEmptySlots > 1) {
@@ -67,12 +77,42 @@ public class Board extends TilePane {
     }
 
     private void checkIfOtherMoveAvailable() {
+        if (!Model.checkPossibleMerge(tileArray))
+        {
+            gameOver();
+        }
+    }
 
+    private void gameOver() {
+    }
+
+    public void moved(Direction key) {
+        switch (key.getCode()) {
+            case UP:
+                moved(UP);
+                break;
+            case DOWN:
+                moved(DOWN);
+                break;
+            case LEFT:
+                moved(LEFT);
+                break;
+            case RIGHT:
+                moved(Direction.RIGHT);
+                break;
+            case O:
+                gameOver();
+                break;
+            default:
+                break;
+        }
     }
 
     private ObservableList<Slot> getSlotsWithEmptyTiles() {
         return null;
     }
+
+
 
 
 }
---
+++ src/main/java/src/Direction.java
@@ -0,0 +1,6 @@
+package src;
+
+public enum Direction {
+    UP, DOWN, LEFT, RIGHT;
+
+}
