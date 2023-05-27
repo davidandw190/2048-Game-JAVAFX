2f36e358e767115f1ba397ed39622bf85e3052af
Nan Andrei David <andrei.nan03@e-uvt.ro>
2023-05-27 20:58:55 +0300

    Started implementing the actual Game running method;

--- src/main/java/src/Game.java
+++ src/main/java/src/Game.java
@@ -1,18 +1,55 @@
 package src;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Game extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-
-        Board board = new Board();
-    }
 
     public static void main(String[] args) {
         launch();
     }
+
+    @Override
+    public void start(Stage window) throws Exception {
+
+
+        /* The Stage is the window. The Scene is what happens inside the window. The scene has a child node of type Group called root. */
+
+        Board board = new Board();
+
+        StackPane sp = new StackPane(board);
+        Group root = new Group(); //root for all the children nodes to be added to the scene
+
+        root.getChildren().addAll(sp);
+
+        final int WIDTH = 503;
+        final int HEIGHT = 503;
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+
+        window.setScene(scene);
+        scene.getStylesheets().add("stylesheet.css"); //adds the styles in stylesheet.css to game
+
+        scene.setOnKeyPressed(ke -> board.moved(ke));
+
+        board.setOnSwipeUp(se -> board.moved(Direction.UP));
+        board.setOnSwipeDown(se -> board.moved(Direction.DOWN));
+        board.setOnSwipeLeft(se -> board.moved(Direction.LEFT));
+        board.setOnSwipeRight(se -> board.moved(Direction.RIGHT));
+
+
+
+
+
+        window.setTitle("2048 | Java");
+
+        window.setResizable(false);
+
+        window.show();
+    }
+}
 }
